f952c233d77c5f62b509a4a0286af52212282486
alexey-mr <alexey.morlang@gmail.com>
2016-03-22 17:21:50 +0300

    Used define sds instead of class

--- deployment_scripts/puppet/manifests/compute.pp
+++ deployment_scripts/puppet/manifests/compute.pp
@@ -12,13 +12,19 @@ if $scaleio['metadata']['enabled'] {
     class {'scaleio_openstack::nova':
       ensure  => present,
     }
-    if $::sds_ips {
+    $fuel_version = hiera('fuel_version')
+    $all_nodes = hiera('nodes')
+    $nodes  = filter_nodes($all_nodes, 'name', $::hostname)
+    $hashes = nodes_to_hash($nodes, 'name', 'storage_address')
+    $ips    = ipsort(values($hashes))
+    $node_ips = split(regsubst($::ip_address_array, '[ "\[\]]', '', 'G'), ',')
+    $sds_ips  = intersection($ips, $node_ips)    
+    if $sds_ips {
       #use first ip as SDS name
-      $sds_name = $::sds_ips[0]
+      $sds_name = $sds_ips[0]
       # remove possible trailing comas
-      $ips = join(split($::sds_ips, ','), ',')
       # generate array of roles (all) with lenght of ips
-      $roles = join(values(hash(split(regsubst("${ips},", ',', ',all,', 'G'), ','))), ',')
+      $ip_roles = join(values(hash(split(regsubst("${sds_ips},", ',', ',all,', 'G'), ','))), ',')
       $paths = $scaleio['device_paths'] ? {
         udnef   => undef,
         default => join(split($scaleio['device_paths'], ','), ',')
@@ -33,16 +39,16 @@ if $scaleio['metadata']['enabled'] {
       }
       notify {"Devices ${device_paths}": } ->
       notify {"Storage pools ${storage_pools}": } ->
-      notify {"IPs and roles ${ips} /  ${roles}": } ->
-      class {'scaleio::sds':
+      notify {"IPs and roles ${sds_ips} /  ${ip_roles}": } ->
+      scaleio::sds {"Configure SDS ${sds_name}":
         ensure             => 'present',
         ensure_properties  => undef,
         name               => $sds_name,
         protection_domain  => $scaleio['protection_domain'],
         fault_set          => undef,
         port               => undef,
-        ips                => $ips,             # "1.2.3.4,1.2.3.5"
-        ip_roles           => $roles,           # "all,all"
+        ips                => $sds_ips,         # "1.2.3.4,1.2.3.5"
+        ip_roles           => $ip_roles,           # "all,all"
         storage_pools      => $storage_pools,   # "sp1,sp2"
         device_paths       => $device_paths,    # "/dev/sdb,/dev/sdc",
       }
