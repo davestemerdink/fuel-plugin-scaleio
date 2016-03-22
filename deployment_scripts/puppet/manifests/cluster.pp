94127cd786cfd3b8a29e1d97205f6a7f4c5971ab
alexey-mr <alexey.morlang@gmail.com>
2016-03-23 00:08:42 +0300

    Fixed SDS configurations.

--- deployment_scripts/puppet/manifests/cluster.pp
+++ deployment_scripts/puppet/manifests/cluster.pp
@@ -43,6 +43,39 @@ define ensure_password($old_password, $password) {
   scaleio::login {'Normal': password => $password }
 }
 
+define ensure_sds(
+  $protection_domain,
+  $ips,
+  $ip_roles,
+  $storage_pools,
+  $device_paths,
+) {
+  $sds_node_name = $title
+  $sds_name = "sds-${sds_node_name}"  
+  $all_nodes = hiera('nodes')
+  $nodes = filter_nodes($all_nodes, 'name', $sds_node_name)
+  $hashes = nodes_to_hash($nodes, 'name', 'storage_address')
+  $sds_ips = ipsort(values($hashes))
+  $sds_ips_str = join($sds_ips, ',')
+  # remove possible trailing comas
+  # generate array of roles (all) with lenght of ips
+  $ip_roles = join(values(hash(split(regsubst("${sds_ips_str},", ',', ',all,', 'G'), ','))), ',')
+  notify {"IPs and roles ${sds_ips_str} /  ${ip_roles}": } ->
+  scaleio::sds {$sds_name:
+    ensure             => 'present',
+    ensure_properties  => undef,
+    name               => $sds_name,
+    protection_domain  => $protection_domain,
+    fault_set          => undef,
+    port               => undef,
+    ips                => $sds_ips_str,
+    ip_roles           => $ip_roles,
+    storage_pools      => $storage_pools,
+    device_paths       => $device_paths,
+    require            => Scaleio::Login['login'],
+  }
+}
+
 # The only first mdm which is proposed to be the first master does cluster configuration
 $scaleio = hiera('scaleio')
 if $scaleio['metadata']['enabled'] {
@@ -62,6 +95,20 @@ if $scaleio['metadata']['enabled'] {
         default => $env_password
       }
       $password = $scaleio['password']
+      $all_nodes = hiera('nodes')
+      $compute_nodes  = filter_nodes($all_nodes, 'role', 'compute')
+      $paths = $scaleio['device_paths'] ? {
+        udnef   => undef,
+        default => join(split($scaleio['device_paths'], ','), ',') # remove possible trailing comas
+      }
+      if $paths and count($paths) > 0 {
+        #generate array of pools with lenght of device_paths
+        $device_paths = $paths
+        $storage_pools = join(values(hash(split(regsubst("${device_paths},", ',', ",${scaleio['storage_pool']},", 'G'), ','))), ',')
+      } else {
+        $device_paths = undef
+        $storage_pools = undef
+      }
       notify {"Master MDM ${master_ip}": } ->
       class {'scaleio::mdm_server':
         ensure              => 'present',
@@ -78,6 +125,13 @@ if $scaleio['metadata']['enabled'] {
         cluster_mode        => $cluster_mode,
         slave_names         => $slave_names,
         tb_names            => $tb_names,
+      } ->
+      notify {"Devices ${device_paths}": } ->
+      notify {"Storage pools ${storage_pools}": } ->
+      ensure_sds {$compute_nodes:
+        protection_domain => $scaleio['protection_domain'],
+        storage_pools     => $storage_pools,
+        device_paths      => $device_paths,
       }
     } else {
       notify {"Not Master MDM ${master_ip}": }
