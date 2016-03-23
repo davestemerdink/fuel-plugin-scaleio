885c5cebdb095d35abc9153973324f305b6d3610
alexey-mr <alexey.morlang@gmail.com>
2016-03-23 12:10:56 +0000

    Pass arrays into cinder puppet instad of strings.

--- deployment_scripts/puppet/manifests/cinder.pp
+++ deployment_scripts/puppet/manifests/cinder.pp
@@ -2,6 +2,8 @@
 $scaleio = hiera('scaleio')
 if $scaleio['metadata']['enabled'] {
   if $::gateway_ips {
+    $protection_domains = split($scaleio['protection_domain'], ',')
+    $storage_pools = split($scaleio['storage_pool'], ',')
     class {'scaleio_openstack::cinder':
       ensure                     => present,
       gateway_user               => 'admin',
@@ -9,16 +11,6 @@ if $scaleio['metadata']['enabled'] {
       gateway_ip                 => $::gateway_ips,
       protection_domains         => $scaleio['protection_domain'],
       storage_pools              => $scaleio['storage_pool'],
-    } ->
-    class {'scaleio_openstack::volume_type':
-      ensure              => present,
-      protection_domains  => [$scaleio['protection_domain']],
-      storage_pools       => [$scaleio['storage_pool']],
-      provisioning        => ['thin'],
-      os_password         => $::os_password,
-      os_tenant_name      => $::os_tenant_name,
-      os_username         => $::os_username,
-      os_auth_url         => $::os_auth_url,
     }
   } else {
     fail('Empty Gateway IPs configuration')
