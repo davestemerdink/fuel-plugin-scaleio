dd552b3297e136753e5cff45cb5d596f3c3d411d
alexey-mr <alexey.morlang@gmail.com>
2016-03-23 13:23:40 +0000

    Add check taht cinder is installed on controller

--- deployment_scripts/puppet/manifests/cinder.pp
+++ deployment_scripts/puppet/manifests/cinder.pp
@@ -2,6 +2,11 @@
 $scaleio = hiera('scaleio')
 if $scaleio['metadata']['enabled'] {
   if $::gateway_ips {
+    $all_nodes = hiera('nodes')
+    $nodes = filter_nodes($all_nodes, 'name', $::hostname)
+    if empty(filter_nodes($nodes, 'role', 'cinder')) {
+      fail('In order to use ScaleIO it is required to isntall Cinder Role on all controller nodes.')
+    }
     $protection_domains = split($scaleio['protection_domain'], ',')
     $storage_pools = split($scaleio['storage_pool'], ',')
     class {'scaleio_openstack::cinder':
@@ -9,8 +14,8 @@ if $scaleio['metadata']['enabled'] {
       gateway_user               => 'admin',
       gateway_password           => $scaleio['gateway_password'],
       gateway_ip                 => $::gateway_ips,
-      protection_domains         => $scaleio['protection_domain'],
-      storage_pools              => $scaleio['storage_pool'],
+      protection_domains         => $protection_domains,
+      storage_pools              => $storage_pools,
     }
   } else {
     fail('Empty Gateway IPs configuration')
