3999a9ba664a4a117f1d5d7f41e8cb1cab515d5a
alexey-mr <alexey.morlang@gmail.com>
2016-03-25 12:47:04 +0000

    Connect each cinder to own gateway. This is until HA is not implemented.

--- deployment_scripts/puppet/manifests/cinder.pp
+++ deployment_scripts/puppet/manifests/cinder.pp
@@ -7,11 +7,20 @@ if $scaleio['metadata']['enabled'] {
     if empty(filter_nodes($nodes, 'role', 'cinder')) {
       fail('In order to use ScaleIO it is required to isntall Cinder Role on all controller nodes.')
     }
+    
+    #TODO: after HA impl replace gw_ip  with  hiera('management_vip')
+    #      for now just use own gateway or GW on master-controller 
+    $possible_ips = intersection(split($::gateway_ips, ','), split($::ip_address_array, ','))
+    if count($possible_ips) > 0 {
+      $gw_ip = $possible_ips[0]
+    } else {
+      $gw_ip = $::gateway_ips[0]
+    }
     class {'scaleio_openstack::cinder':
       ensure                     => present,
       gateway_user               => 'admin',
       gateway_password           => $scaleio['gateway_password'],
-      gateway_ip                 => $::gateway_ips,
+      gateway_ip                 => $gw_ip,
       protection_domains         => $scaleio['protection_domain'],
       storage_pools              => $scaleio['storage_pools'],
     }
