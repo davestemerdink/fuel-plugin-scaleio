4592d3145b9631002194dc13f41c29b12fe4c7aa
alexey-mr <alexey.morlang@gmail.com>
2016-03-27 19:33:10 +0000

    Run cluster task on all controllers, because IP of mrimary-controller could be different from the first in mdm_ips list. There are additional changes because of change in interface of scaleio_openstack.

--- deployment_scripts/puppet/manifests/compute.pp
+++ deployment_scripts/puppet/manifests/compute.pp
@@ -1,13 +1,22 @@
 $scaleio = hiera('scaleio')
 if $scaleio['metadata']['enabled'] {
   if $::mdm_ips {
+    $gw_ips = split($::gateway_ips, ',')
+    $gateway_ip = $gw_ips[0] #TODO: replace with management_vip after HA be implemented
+
     class {'scaleio::sdc_server':
       ensure  => 'present',
       mdm_ip  => $::mdm_ips,
     } ->
     class {'scaleio_openstack::nova':
-      ensure  => present,
-    }
+      ensure              => present,
+#      gateway_user        => admin, #default
+      gateway_password    => $scaleio['gateway_password'],
+      gateway_ip          => $gateway_ip,
+#      gateway_port        => 4443, #default
+      protection_domains  => $scaleio['protection_domain'],
+      storage_pools       => $scaleio['storage_pools'],
+   }
   } else {
     fail('Empty MDM IPs configuration')
   }
