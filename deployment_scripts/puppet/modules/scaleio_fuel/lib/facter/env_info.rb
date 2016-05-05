56c4851dc618d8b69eb2465cfcfc11d1fa9454bb
alexey-mr <alexey.morlang@gmail.com>
2016-05-05 12:36:18 +0300

    Revert "Fixed request of existing mdm in case of existing cluster"
    This reverts commit f2347582c8fa96408f58402dc73960b98d37d2a6.

--- deployment_scripts/puppet/modules/scaleio_fuel/lib/facter/env_info.rb
+++ deployment_scripts/puppet/modules/scaleio_fuel/lib/facter/env_info.rb
@@ -33,7 +33,7 @@ if $gw_passw && $gw_passw != '' and $gw_ips and $gw_ips != ''
       base_url    = "https://%s:%s/api/%s"
       login_url   = base_url % [host, port, 'login']
       config_url  = base_url % [host, port, 'Configuration']
-      login_req   = "curl -k --basic --connect-timeout 5 --user #{user}:#{gw_passw} #{login_url} 2>>%s | sed 's/\"//g'" % $scaleio_log_file
+      login_req   = "curl -k --basic --connect-timeout 5 --user #{user}:#{$gw_passw} #{login_url} 2>>%s | sed 's/\"//g'" % $scaleio_log_file
       debug_log(login_req)
       token       = Facter::Util::Resolution.exec(login_req)
       if token && token != ''
