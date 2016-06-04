18f805cf4875f849873813249ee57d0fae275e37
alexey-mr <alexey.morlang@gmail.com>
2016-06-04 17:14:25 +0300

    fuxed gateway address for existing cluster case

--- deployment_scripts/puppet/manifests/nova.pp
+++ deployment_scripts/puppet/manifests/nova.pp
@@ -12,11 +12,15 @@ if $scaleio['metadata']['enabled'] {
   } else {
     $provisioning_type = undef
   }
+  $gateway_ip = $scaleio['existing_cluster'] ? {
+    true  => $scaleio['gateway_ip'],
+    default => hiera('management_vip')
+  }
   class {'scaleio_openstack::nova':
     ensure              => present,
     gateway_user        => $::gateway_user,
     gateway_password    => $scaleio['password'],
-    gateway_ip          => hiera('management_vip'),
+    gateway_ip          => $gateway_ip,
     gateway_port        => $::gateway_port,
     protection_domains  => $scaleio['protection_domain'],
     storage_pools       => $::storage_pools,
